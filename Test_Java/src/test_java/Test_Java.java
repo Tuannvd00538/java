5bf17ea1a96f543f5374fd5f85c45ed3d162ba12
Ngô Văn Tuấn <tuannvd00538@fpt.edu.vn>
2018-05-18 22:27:23 +0700

    thảo não cá vàng :v

---
+++ Test_Java/src/test_java/Test_Java.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test_java;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Computer
+ */
+public class Test_Java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//        Câu 6
+        Scanner input = new Scanner(System.in);
+//        System.out.println("Nhập chuỗi cần check:");
+//        String ip = input.nextLine();
+//        char[] stringCheck = ip.toCharArray();
+//        boolean siin = false;
+//        for (char c : stringCheck) {
+//            if (Character.isUpperCase(c)) {
+//                siin = false;
+//                break;
+//            } else {
+//                siin = true;
+//            }
+//        }
+//        System.out.println(siin);
+        
+        
+//        câu 5
+//        System.out.println("Nhập chuỗi thứ nhất:");
+//        String string1 = input.nextLine();
+//        System.out.println("Nhập chuỗi thứ hai:");
+//        String string2 = input.nextLine();
+//        boolean siin = false;
+//        if (string1.contains(string2)) {
+//            siin = true;
+//        }
+//        System.out.println(siin);
+    }
+    
+}
